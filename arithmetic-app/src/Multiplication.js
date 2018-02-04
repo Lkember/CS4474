d0a80146e7eeee9b9d6e787d0e5526841f564715
Jose Rivera <jrivera4@uwo.ca>
2018-02-04 01:43:10 -0500

    Initial Push

---
+++ arithmetic-app/src/Multiplication.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router';
+import logo from './logo.png';
+import './App.css';
+
+class Multiplication extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">This will hold MULT game</h1>
+                </header>
+                <li><Link to="/GameSelection">BACK</Link></li>
+                <li><Link to="/GameSelection">1</Link></li>
+                <li><Link to="/GameSelection">2</Link></li>
+                <li><Link to="/GameSelection">3</Link></li>
+                <li><Link to="/GameSelection">4</Link></li>
+            </div>
+        );
+    }
+}
+
+export default Multiplication;
